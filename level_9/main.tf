06f74d2784e27bd1a40c47ac3b500bb92d6521fa
evairmarinho <evairmarinho@hotmail.com>
2022-01-07 22:58:59 -0300

    count => for_each

--- level_9/main.tf
+++ level_9/main.tf
@@ -1,7 +1,7 @@
 module "s3" {
   source                  = "./modules/s3/"
-  count                   = length(var.s3_bucket_names)
-  bucket_name             = var.s3_bucket_names[count.index]
+  for_each                = toset(var.s3_bucket_names)
+  bucket_name             = each.key
   block_public_acls       = var.s3_block_public_acls
   block_public_policy     = var.s3_block_public_policy
   ignore_public_acls      = var.s3_ignore_public_acls
@@ -12,5 +12,5 @@ module "ec2" {
   source        = "./modules/ec2/"
   instance_type = var.ec2_instance_type
   instance_name = var.ec2_instance_name
-  bucket_arns   = module.s3.*.bucket_arn
+  bucket_arns   = [for i in module.s3 : i.bucket_arn]
 }
--- level_9/outputs.tf
+++ level_9/outputs.tf
@@ -1,11 +1,11 @@
 output "s3_bucket_names" {
   description = "S3 bucket name."
-  value       = [for i in module.s3.*.bucket_name : i]
+  value       = [for i in module.s3 : i.bucket_name]
 }
 
 output "s3_bucket_arns" {
   description = "S3 bucket ARN."
-  value       = [for i in module.s3.*.bucket_arn : i]
+  value       = [for i in module.s3 : i.bucket_arn]
 }
 
 output "ec2_instance_id" {
